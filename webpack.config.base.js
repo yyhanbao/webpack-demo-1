9da56961769f620ad7a9fcd3fc9717520af9b8f2
yaoyao <iduting@163.com>
2022-10-19 21:10:20 +0800

    准备上传了

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,4 +12,33 @@ module.exports = {
       template: "src/assets/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"], // 把文件转为文件路径
+      },
+      {
+        test: /\.styl$/,
+        use: ["style-loader", "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
