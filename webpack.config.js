56f2971b96185652c35b81fe1fa111462bc4b2ab
yaoyao <iduting@163.com>
2022-10-19 16:26:15 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "development", // 理解 mode 模式
   entry: "./src/index.js",
   output: {
-    filename: "[name].[contenthash].js",
+    filename: "index.[contenthash].js", // 自动更新文件名
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "写代码啦",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
